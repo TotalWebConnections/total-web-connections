aafa7577ba0c6c574a5d29f9373b0dde266a921e
peterjewicz <peterjewicz@totalwebconnections.com>
2021-08-27 22:17:38 -0500

    Callout section

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,6 +29,12 @@ const BlogIndex = ({ data, location }) => {
     <Layout location={location} title={siteTitle}>
       <Seo title="Total Web Connections" />
       <Hero />
+      <div className="text-center py-28 max-w-5xl m-auto px-4 md:px-0">
+        <h2 className="text-5xl pb-8">SOFTWARE. WEBSITES. APPS.</h2>
+        <h4 className="text-xl pb-8 border-b-2 border-secondary">
+          Total Web Connections is a bespoke software development partener in the Chicago suburbs. We offer services to small and mid sized businsses looking to streamline their operations through custom software.
+        </h4>
+      </div>
       <ol style={{ listStyle: `none` }}>
         {posts.map(post => {
           const title = post.frontmatter.title || post.fields.slug
