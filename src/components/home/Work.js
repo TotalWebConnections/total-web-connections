3f195814171010a062f39728edd09851807f9219
peterjewicz <peterjewicz@totalwebconnections.com>
2021-11-09 15:32:59 -0600

    Modal stuff

--- src/components/home/Work.js
+++ src/components/home/Work.js
@@ -3,23 +3,30 @@ import { Link } from "gatsby"
 import { projects } from '../../portfolio/projects'
 import WorkModal from './WorkModal'
 
-
 const Work = () => {
+  console.log(projects)
   const [modalOpen, setModalOpen] = React.useState(false)
+  const [activeProject, setActiveProject] = React.useState(null)
+
+  const onProjectClick = (id) => {
+    setModalOpen(true)
+    setActiveProject(projects[id])
+  }
+
   return (
     <div>
-      <WorkModal active={modalOpen} onClose={setModalOpen}/>
+      <WorkModal activeProject={activeProject} active={modalOpen} onClose={setModalOpen}/>
       <div className="max-w-4xl mx-auto py-28">
         <h2 className="text-4xl">What We've Done</h2>
         <div className="grid grid-cols-3">
-          <div onClick={() => setModalOpen(true)} >
-            <p>Lifting buddy - Mobile app built and deployed on iOS</p>
+          <div onClick={() => onProjectClick(1)} >
+            <p>The Indoor Gardens - Gardening Focus Static Blog</p>
           </div>
-          <div>
+          <div onClick={() => onProjectClick(0)}>
             <p>Simple Lead Tracker - Simple to use CMS for freelancers</p>
           </div>
-          <div>
-            <p>Worlds Crafter - World building software for everyone</p>
+          <div onClick={() => onProjectClick(2)}>
+            <p>Narrative Planner - World building software for everyone</p>
           </div>
         </div>
       </div>
--- src/components/home/WorkModal.js
+++ src/components/home/WorkModal.js
@@ -3,15 +3,24 @@ import { Link } from "gatsby"
 import { projects } from '../../portfolio/projects'
 
 
-const WorkModal = ({active, onClose}) => {
+const WorkModal = ({active, onClose, activeProject}) => {
 
   return (
     <div
       className={`WorkModal fixed h-full w-full flex justify-center items-center ${active ? "active" : null}`}
       style={{background: "rgba(6,22,36,0.65)"}}
     >
-      <div className="WorkModal__inner h-3/4 bg-white w-9/12">
+      <div className="WorkModal__inner h-3/4 bg-white w-9/12 p-4">
         <p onClick={() => onClose(false)}>X</p>
+        <div class="grid grid-cols-2 gap-4">
+          <div>
+            <h2 className="text-2xl">{ activeProject?.name }</h2>
+            <p className="text-lg">{ activeProject?.description }</p>
+          </div>
+          <div>
+            <img src={`/images/portfolio/${activeProject?.image}`} width="90%" />
+          </div>
+        </div>
       </div>
     </div>
   )
