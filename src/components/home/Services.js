14ceec408b17a60ed7bf085f7507451941ae7968
peterjewicz <peterjewicz@totalwebconnections.com>
2022-11-15 08:59:56 -0600

    new services section

--- src/components/home/Services.js
+++ src/components/home/Services.js
@@ -31,9 +31,9 @@ const Services = () => {
   return (
     <div className="mt-32 py-24 bg-gray">
       <h2 className="text-4xl font-bold text-center">Our Services</h2>
-      <div className="grid md:grid-cols-3 max-w-4xl mx-auto">
+      <div className="grid md:grid-cols-3 max-w-5xl mx-auto">
         <div className="service-card mx-auto">
-          <i class="text-primary text-4xl fa fa-desktop"></i>
+          <i class="text-primary text-4xl fa fa-desktop mt-3 mb-3"></i>
           <h3 className="py-3 text-2xl">
             Web Development
           </h3>
@@ -42,7 +42,7 @@ const Services = () => {
           </p>
         </div>
         <div className="service-card">
-          <i class="text-primary text-4xl fa fa-database"></i>
+          <i class="text-primary text-4xl fa fa-database mt-3 mb-3"></i>
           <h3 className="py-3 text-2xl">
             App Development
           </h3>
@@ -51,13 +51,33 @@ const Services = () => {
           </p>
         </div>
         <div className="service-card">
-          <i class="text-primary text-4xl fa fa-mobile"></i>
+          <i class="text-primary text-4xl fa fa-mobile mt-3 mb-3"></i>
           <h3 className="py-3 text-2xl">
             Mobile Applications
           </h3>
           <p>
             Dedicated solutions for mobile devices. Custom designed to work on mobile devices.
           </p>
+        </div>
+      </div>
+      <div className="grid md:grid-cols-2 max-w-4xl mx-auto">
+        <div className="service-card">
+            <i class="text-primary text-4xl fa fa-scroll mt-3 mb-3"></i>
+            <h3 className="py-3 text-2xl">
+              Content Marketing
+            </h3>
+            <p>
+              Reach new users with handwritten content designed to get clicks.
+            </p>
+          </div>
+          <div className="service-card">
+            <i class="text-primary text-4xl fa fa-thumbs-up mt-3 mb-3"></i>
+            <h3 className="py-3 text-2xl">
+              Social Media Marketing
+            </h3>
+            <p>
+              Build communities with bespoke strategies across social channels.
+            </p>
         </div>
       </div>
     </div>
