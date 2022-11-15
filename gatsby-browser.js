925fcfffbd8d088d3b2b4e10aa4db0d6e9f175eb
peterjewicz <peterjewicz@totalwebconnections.com>
2022-11-15 09:07:40 -0600

    new font

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,6 +5,8 @@ import "typeface-merriweather"
 import "@fontsource/open-sans"
 import "@fontsource/lato"
 import "@fontsource/lato/700.css"
+import "@fontsource/fira-sans"
+import "@fontsource/fira-sans/900.css"
 // normalize CSS across browsers
 import "./src/normalize.css"
 // custom CSS styles
