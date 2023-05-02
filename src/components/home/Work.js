60413e69a07cffdcbfb55f585de4c46d4497c8d1
peterjewicz <peterjewicz@totalwebconnections.com>
2023-05-02 16:20:56 -0500

    Small update

--- src/components/home/Work.js
+++ src/components/home/Work.js
@@ -1,72 +1,99 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import { projects } from '../../portfolio/projects'
-import WorkModal from './WorkModal'
+import { projects } from "../../portfolio/projects"
+import WorkModal from "./WorkModal"
 
 const Work = () => {
   const [modalOpen, setModalOpen] = React.useState(false)
   const [activeProject, setActiveProject] = React.useState(null)
 
-  const onProjectClick = (id) => {
+  const onProjectClick = id => {
     setModalOpen(true)
     setActiveProject(projects[id])
   }
 
   return (
     <div>
-      <WorkModal activeProject={activeProject} active={modalOpen} onClose={setModalOpen}/>
+      <WorkModal
+        activeProject={activeProject}
+        active={modalOpen}
+        onClose={setModalOpen}
+      />
       <div className="mx-auto pt-28">
-        <h2 className="text-4xl font-bold text-center">What We've Done</h2>
-        <p className="text-center mb-4 text-xl">A sample sample of the projects we've worked and the brands we own.</p>
+        <h2 className="text-4xl font-bold text-center">Our Work</h2>
+        <p className="text-center mb-4 text-xl">
+          A small sample of the apps and sites we own and market.
+        </p>
         <div className="pt-8 grid grid-cols-1 md:grid-cols-3">
-          <div className="WorkItem bg-white transition-all"  onClick={() => onProjectClick(1)} >
+          <div
+            className="WorkItem bg-white transition-all"
+            onClick={() => onProjectClick(1)}
+          >
             <div className="overflow-hidden relative">
               <img src="/images/portfolio/indoor-gardens.jpg" width="100%" />
-              
-              <div 
+
+              <div
                 className="WorkItem__content px-2 text-center absolute top-0 text-black h-full w-full flex flex-col justify-center"
-                style={{background: "rgba(0,0,0,.75)"}}
-                >
-                <p className="text-white text-lg font-bold">The Indoor Gardens - Gardening Focus Static Blog</p>
+                style={{ background: "rgba(0,0,0,.75)" }}
+              >
+                <p className="text-white text-lg font-bold">
+                  The Indoor Gardens - Gardening Focus Content Site
+                </p>
               </div>
             </div>
           </div>
-          <div className="WorkItem md:mt-0 bg-white transition-all hover:text-white"   onClick={() => onProjectClick(0)}>
+          <div
+            className="WorkItem md:mt-0 bg-white transition-all hover:text-white"
+            onClick={() => onProjectClick(0)}
+          >
             <div className="overflow-hidden relative">
-              <img src="/images/portfolio/simple-lead-tracker.jpg" width="100%" />
-              <div 
+              <img
+                src="/images/portfolio/simple-lead-tracker.jpg"
+                width="100%"
+              />
+              <div
                 className="WorkItem__content px-2 text-center absolute top-0 text-black h-full w-full flex flex-col justify-center"
-                style={{background: "rgba(50,50,50,.55)"}}
-                >
-                <p className="text-white text-lg font-bold">Simple Lead Tracker - Simple to use CMS for freelancers</p>
+                style={{ background: "rgba(50,50,50,.55)" }}
+              >
+                <p className="text-white text-lg font-bold">
+                  Simple Lead Tracker - Simple to use CMS for freelancers
+                </p>
               </div>
             </div>
           </div>
-          <div className="WorkItem md:mt-0 bg-white transition-all hover:text-white"  onClick={() => onProjectClick(2)}>
+          <div
+            className="WorkItem md:mt-0 bg-white transition-all hover:text-white"
+            onClick={() => onProjectClick(2)}
+          >
             <div className="overflow-hidden relative">
               <img src="/images/portfolio/narrative-planner.jpg" width="100%" />
-              <div 
+              <div
                 className="WorkItem__content px-2 text-center absolute top-0 text-black h-full w-full flex flex-col justify-center"
-                style={{background: "rgba(0,0,0,.75)"}}
-                >
-                <p className="text-white text-lg font-bold">Narrative Planner - World building software for everyone</p>
+                style={{ background: "rgba(0,0,0,.75)" }}
+              >
+                <p className="text-white text-lg font-bold">
+                  Narrative Planner - World building software for everyone
+                </p>
               </div>
             </div>
           </div>
 
-          <div className="WorkItem md:mt-0 bg-white transition-all hover:text-white"  onClick={() => onProjectClick(3)}>
+          <div
+            className="WorkItem md:mt-0 bg-white transition-all hover:text-white"
+            onClick={() => onProjectClick(3)}
+          >
             <div className="overflow-hidden relative">
-              <img src="/images/portfolio/narrative-planner.jpg" width="100%" />
-              <div 
+              <img src="/images/portfolio/diy-guide.jpg" width="100%" />
+              <div
                 className="WorkItem__content px-2 text-center absolute top-0 text-black h-full w-full flex flex-col justify-center"
-                style={{background: "rgba(0,0,0,.75)"}}
-                >
-                <p className="text-white text-lg font-bold">Homeowners DIY Guide - Blog For First Time Homebuyers</p>
+                style={{ background: "rgba(0,0,0,.75)" }}
+              >
+                <p className="text-white text-lg font-bold">
+                  Homeowners DIY Guide - Blog For First Time Homebuyers
+                </p>
               </div>
             </div>
           </div>
-
-
         </div>
       </div>
     </div>
--- src/components/home/WorkModal.js
+++ src/components/home/WorkModal.js
@@ -1,34 +1,43 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import { projects } from '../../portfolio/projects'
+import { projects } from "../../portfolio/projects"
 
-
-const WorkModal = ({active, onClose, activeProject}) => {
-
-
+const WorkModal = ({ active, onClose, activeProject }) => {
   return (
     <div
-      className={`WorkModal fixed h-full w-full flex justify-center items-center ${active ? "active" : null}`}
-      style={{background: "rgba(6,22,36,0.65)"}}
+      className={`WorkModal fixed h-full w-full flex justify-center items-center ${
+        active ? "active" : null
+      }`}
+      style={{ background: "rgba(6,22,36,0.65)" }}
     >
       <div className="WorkModal__inner h-3/4 bg-white w-9/12 p-4 rounded-md overflow-scroll">
-        <p className="text-right text-bold cursor-pointer transition hover:text-primary text-bold text-xl" onClick={() => onClose(false)}>Close X</p>
+        <p
+          className="text-right text-bold cursor-pointer transition hover:text-primary text-bold text-xl"
+          onClick={() => onClose(false)}
+        >
+          Close X
+        </p>
         <div class="grid md:grid-cols-2 gap-4 pt-4">
           <div>
-            <h2 className="text-2xl font-bold">{ activeProject?.name }</h2>
-            <p className="text-lg py-4">{ activeProject?.description }</p>
-            <div>
-             <h2 className="text-xl font-bold pt-4">What We Did</h2>
-             <ul className="pl-2 pt-2">
-              {activeProject?.services.map((service) => <li>- {service}</li>)}
-             </ul>
-            </div>
+            <h2 className="text-2xl font-bold">{activeProject?.name}</h2>
+            <p className="text-lg py-4">{activeProject?.description}</p>
             <div className="pt-6">
-              <a className="text-primary hover:text-black transition-all" href={activeProject?.url} target="_blank"><button className="text-bold text-xl">{activeProject?.name}</button></a>
+              <a
+                className="text-primary hover:text-black transition-all"
+                href={activeProject?.url}
+                target="_blank"
+              >
+                <button className="text-bold text-xl">
+                  {activeProject?.name}
+                </button>
+              </a>
             </div>
           </div>
           <div>
-            <img src={`/images/portfolio/${activeProject?.image}`} width="90%" />
+            <img
+              src={`/images/portfolio/${activeProject?.image}`}
+              width="90%"
+            />
           </div>
         </div>
       </div>
