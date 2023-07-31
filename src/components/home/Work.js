85b9ae7e1f4373a953ebef058071f5d5bdacc070
peterjewicz <peterjewicz@totalwebconnections.com>
2023-07-31 14:53:53 -0500

    Setup new project display

--- src/components/home/Work.js
+++ src/components/home/Work.js
@@ -14,100 +14,85 @@ const Work = () => {
 
   return (
     <div>
-      <WorkModal
-        activeProject={activeProject}
-        active={modalOpen}
-        onClose={setModalOpen}
-      />
       <div className="mx-auto pt-28">
         <h2 className="text-4xl font-bold text-center">Our Work</h2>
         <p className="text-center mb-4 text-xl">
           A small sample of the apps and sites we own and market.
         </p>
-        <div className="pt-8 grid grid-cols-1 md:grid-cols-3">
+        <div>
           <div
-            className="WorkItem bg-white transition-all"
-            onClick={() => onProjectClick(1)}
+            className=" bg-gray transition-all Card w-3/4 mx-auto p-16 rounded-3xl border my-16"
+            style={{ borderColor: "#c7cdce" }}
           >
-            <div className="overflow-hidden relative">
-              <img src="/images/portfolio/indoor-gardens.jpg" width="100%" />
-
-              <div
-                className="WorkItem__content px-2 text-center absolute top-0 text-black h-full w-full flex flex-col justify-center"
-                style={{ background: "rgba(0,0,0,.75)" }}
-              >
-                <p className="text-white text-lg font-bold">
-                  The Indoor Gardens - Gardening Focus Content Site
-                </p>
+            <div className="block md:flex text-center md:text-left">
+              <img
+                className="rounded-3xl  md:w-3/5 mx-2"
+                src="/images/portfolio/indoor-gardens.jpg"
+                width="100%"
+              />
+              <div className="md:w-2/5 pl-6 pt-4 md:pt-0">
+                <h2 className="text-2xl font-bold pb-4">The Indoor Gardens</h2>
+                <p className="pb-4">Gardening Focused Content Site. </p>
+                <a
+                  className="text-primary"
+                  href="https://www.theindoorgardens.com"
+                  target="_blank"
+                >
+                  Visit Site
+                </a>
               </div>
             </div>
           </div>
+
           <div
-            className="WorkItem md:mt-0 bg-white transition-all hover:text-white"
-            onClick={() => onProjectClick(0)}
+            className=" bg-gray transition-all Card w-3/4 mx-auto p-16 rounded-3xl border my-16"
+            style={{ borderColor: "#c7cdce" }}
           >
-            <div className="overflow-hidden relative">
+            <div className="block md:flex text-center md:text-left">
               <img
+                className="rounded-3xl md:w-3/5 px-2"
                 src="/images/portfolio/simple-lead-tracker.jpg"
                 width="100%"
               />
-              <div
-                className="WorkItem__content px-2 text-center absolute top-0 text-black h-full w-full flex flex-col justify-center"
-                style={{ background: "rgba(50,50,50,.55)" }}
-              >
-                <p className="text-white text-lg font-bold">
-                  Simple Lead Tracker - Simple to use CMS for freelancers
+              <div className="md:w-2/5 pl-6 pt-4 md:pt-0">
+                <h2 className="text-2xl font-bold pb-4">Simple Lead Tracker</h2>
+                <p className="pb-4">
+                  Easy to use CMS built for small businesses and freelancers.
                 </p>
-              </div>
-            </div>
-          </div>
-          <div
-            className="WorkItem md:mt-0 bg-white transition-all hover:text-white"
-            onClick={() => onProjectClick(2)}
-          >
-            <div className="overflow-hidden relative">
-              <img src="/images/portfolio/narrative-planner.jpg" width="100%" />
-              <div
-                className="WorkItem__content px-2 text-center absolute top-0 text-black h-full w-full flex flex-col justify-center"
-                style={{ background: "rgba(0,0,0,.75)" }}
-              >
-                <p className="text-white text-lg font-bold">
-                  Narrative Planner - World building software for everyone
-                </p>
+                <a
+                  className="text-primary"
+                  href="https://www.simpleleadtracker.com"
+                  target="_blank"
+                >
+                  Visit Site
+                </a>
               </div>
             </div>
           </div>
 
           <div
-            className="WorkItem md:mt-0 bg-white transition-all hover:text-white"
-            onClick={() => onProjectClick(3)}
+            className=" bg-gray transition-all Card w-3/4 mx-auto p-16 rounded-3xl border my-16"
+            style={{ borderColor: "#c7cdce" }}
           >
-            <div className="overflow-hidden relative">
-              <img src="/images/portfolio/diy-guide.jpg" width="100%" />
-              <div
-                className="WorkItem__content px-2 text-center absolute top-0 text-black h-full w-full flex flex-col justify-center"
-                style={{ background: "rgba(0,0,0,.75)" }}
-              >
-                <p className="text-white text-lg font-bold">
-                  Homeowners DIY Guide - Blog For First Time Homebuyers
+            <div className="block md:flex text-center md:text-left">
+              <img
+                className="rounded-3xl md:w-3/5 pt-4 md:pt-0"
+                src="/images/portfolio/writeProgress.jpg"
+                width="100%"
+              />
+              <div className="md:w-2/5 pl-6">
+                <h2 className="text-2xl font-bold pb-4">Write Progres</h2>
+                <p className="pb-4">
+                  Web app for helping writers track their writing, stay
+                  consistent, and become better at their craft.
                 </p>
-              </div>
-            </div>
-          </div>
-
-          <div
-            className="WorkItem md:mt-0 bg-white transition-all hover:text-white"
-            onClick={() => onProjectClick(4)}
-          >
-            <div className="overflow-hidden relative">
-              <img src="/images/portfolio/writeProgress.jpg" width="100%" />
-              <div
-                className="WorkItem__content px-2 text-center absolute top-0 text-black h-full w-full flex flex-col justify-center"
-                style={{ background: "rgba(0,0,0,.75)" }}
-              >
-                <p className="text-white text-lg font-bold">
-                  WriteProgress - Writer Focused SaSS App
-                </p>
+                <a
+                  className="text-primary"
+                  href="https://www.writeprogress.com"
+                  target="_blank"
+                >
+                  Visit Site
+                </a>
               </div>
             </div>
           </div>
--- src/pages/work.js
+++ src/pages/work.js
@@ -6,37 +6,41 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 import SubHero from "../components/SubHero"
-import { projects } from '../portfolio/projects'
+import { projects } from "../portfolio/projects"
 
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
-  const copy = "We're proud of our work. Check out some of our previous and ongoing projects.";
+  const copy =
+    "We're proud of our work. Check out some of our previous and ongoing projects."
   return (
     <Layout location={location} title={siteTitle}>
       <Seo title="Total Web Connections | Our Work" />
-      <SubHero header="Our Past Projects" copy={copy}/>
+      <SubHero header="Our Past Projects" copy={copy} />
       <div className="grid grid-cols-1 sm:grid-cols-2 text-white">
         {projects.map(project => {
           return (
             <div
               className="bg-cover bg-center relative overflow-hidden WorkBlock"
               key={project.name}
-              style={{backgroundImage: `url('/images/portfolio/${project.image}')`, height: "230px"}}
+              style={{
+                backgroundImage: `url('/images/portfolio/${project.image}')`,
+                height: "230px",
+              }}
             >
               <Link to={`/work/${project.localUrl}`} itemProp="url">
                 <div
                   className="absolute h-full w-full"
-                  style={{backgroundColor: "rgba(64, 78, 91, 0.9)"}}
-                >
-                </div>
+                  style={{ backgroundColor: "rgba(64, 78, 91, 0.9)" }}
+                ></div>
                 <div className="relative z-10 flex items-center h-full">
-                  <h3 className="text-3xl text-secondary p-4" style={{backgroundColor: "rgba(255, 255, 255, 0.85)"}}>
-                    { project.name }
-                  </h3>
-                  <p
-                    className="absolute transition-all pl-4 WorkBlock__copy"
+                  <h3
+                    className="text-3xl text-secondary p-4"
+                    style={{ backgroundColor: "rgba(255, 255, 255, 0.85)" }}
                   >
-                    { project.excerpt }
+                    {project.name}
+                  </h3>
+                  <p className="absolute transition-all pl-4 WorkBlock__copy">
+                    {project.excerpt}
                   </p>
                 </div>
               </Link>
