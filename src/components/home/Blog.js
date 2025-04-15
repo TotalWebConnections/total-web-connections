99936e478dfd0948afaf12621a985db9205359e7
peterjewicz <peterjewicz@totalwebconnections.com>
2025-04-14 20:20:41 -0500

    small style change for home blog section

--- src/components/home/Blog.js
+++ src/components/home/Blog.js
@@ -4,7 +4,7 @@ import BlogCard from "../BlogCard"
 
 const Blog = ({ posts }) => {
   return (
-    <div className="text-center py-28 max-w-5xl m-auto px-4 md:px-0">
+    <div className="text-center mt-32 py-24 bg-gray">
       <h3 className="text-4xl font-bold text-center">Recent News</h3>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 max-w-5xl m-auto py-16 px-4 md:px-0">
         {posts.map(post => {
